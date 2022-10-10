6742056472b96aa599ae10382413b2efd4180f80
Ahmed Medhat <ahmed.medhat.tawfiq@gmail.com>
2022-10-10 22:48:32 +0200

    Add Array Rules

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,19 @@
 import { RuleCore } from '../core';
-import { LogicalRules, MathRules, ObjectRules } from '../rule';
+import { ArrayRules, LogicalRules, MathRules, ObjectRules } from '../rule';
 
 
 export class JsonLang extends RuleCore {
   private mathRules: MathRules;
   private logicalRules: LogicalRules;
   private objectRules: ObjectRules;
+  private arrayRules: ArrayRules;
 
   constructor() {
     super();
     this.mathRules = new MathRules();
     this.logicalRules = new LogicalRules();
     this.objectRules = new ObjectRules();
+    this.arrayRules = new ArrayRules();
 
     this.registerBuiltinRules();
   }
@@ -20,7 +22,8 @@ export class JsonLang extends RuleCore {
     const mathRules = this.mathRules.getRules();
     const logicalRules = this.logicalRules.getRules();
     const objectRules = this.objectRules.getRules();
+    const arrayRules = this.arrayRules.getRules();
 
-    this.registerMany(new Map([...mathRules, ...logicalRules, ...objectRules]))
+    this.registerMany(new Map([...mathRules, ...logicalRules, ...objectRules, ...arrayRules]))
   }
 }
---
+++ src/rule/array.ts
@@ -0,0 +1,46 @@
+import { IJsonLangParams, RuleInput, Rules, RulesImplementation, Runner } from '../core';
+
+export class ArrayRules implements RulesImplementation {
+
+  private rules: Rules;
+
+  constructor() {
+    this.rules = new Map();
+    this.register();
+  }
+
+  getRules(): Rules {
+    return this.rules;
+  }
+
+  private register = () => {
+    this.rules.set({ name: 'All' }, this.all);
+    this.rules.set({ name: 'Map' }, this.map);
+    this.rules.set({ name: 'Filter' }, this.filter);
+    this.rules.set({ name: 'Flatten' }, this.flatten);
+    this.rules.set({ name: 'Foreach' }, this.foreach);
+  }
+
+  private all = (...inputs: RuleInput[]) => {
+    return [...inputs].map(input => input);
+  }
+
+  private filter = (elements: any[], $rule: IJsonLangParams, $runner: Runner) => {
+    return Array.isArray(elements) ? elements.filter((ele) => $runner(ele)($rule)) : [];
+  }
+
+  private foreach = (elements: any[], $rule: IJsonLangParams, $runner: Runner) => {
+    if (!Array.isArray(elements)) return false;
+
+    elements.forEach((ele) => $runner(ele)($rule));
+  }
+
+  private map = (elements: any[], $rule: IJsonLangParams, $runner: Runner) => {
+    return Array.isArray(elements) ? elements.map((ele) => $runner(ele)($rule)) : [];
+  }
+
+  private flatten = (elements: any[], lvl?: number) => {
+    return Array.isArray(elements) ? elements.flat(lvl) : [];
+  }
+
+}
