c046289101986d2d1d06eb4cee328f32e966ba6b
Ahmed Medhat Tawfiq <ahmed.medhat.tawfiq@gmail.com>
2023-03-17 18:24:39 +0200

    add node by click

--- packages/website/src/pages/flowBuilder/index.jsx
+++ packages/website/src/pages/flowBuilder/index.jsx
@@ -7,7 +7,8 @@ import ReactFlow, {
   Controls,
   Background,
   updateEdge,
-  MiniMap
+  MiniMap,
+  MarkerType
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import Layout from '@theme/Layout';
@@ -18,7 +19,39 @@ import AddButton from './addbutton';
 
 import './index.css';
 
-const initialNodes = [];
+const initialNodes = [
+  {
+    id: '0',
+    type: 'default',
+    data: { label: 'Node' },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '1',
+    type: 'default',
+    data: { label: 'Node' },
+    position: { x: 0, y: 100 },
+  },
+];
+
+const initialEdges = [
+  {
+    id: 'edges-e5-7',
+    source: '0',
+    target: '1',
+    label: '+',
+    labelBgPadding: [8, 4],
+    labelBgBorderRadius: 4,
+    labelBgStyle: { fill: '#FFCC00', color: '#fff', fillOpacity: 0.7 },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  }
+]
+
+const fitViewOptions = {
+  padding: 3,
+};
 
 const nodeTypes = {
   number: Number,
@@ -35,12 +68,66 @@ const getId = () => `dndnode_${id++}`;
 export default () => {
   const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const [addnode, setAddnode] = useState(false);
+  const [addChildeNode, setAddChildeNode] = useState(false);
+  const [parentNode, setParentNode] = useState(null);
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge({ ...params, type: 'step' }, eds)), []);
 
- useEffect(() => {
-    console.log('reactFlowInstance', reactFlowInstance ? reactFlowInstance.toObject() : {})
-  })
+
+  const initialNodeType = {
+    id : getId(),
+    type : 'default',
+    position : { x: initialNodes[0].position.x, y: nodes.length*100},
+    data: { label: 'New Node' },
+    width: 150
+  }
+
+  const initialEdge = {
+    id: String(parseInt(Math.random(100000000)*1000000)),
+    source: nodes[nodes.length-2].id,
+    target: nodes[nodes.length-1].id,
+    label: '+',
+    labelBgPadding: [8, 4],
+    labelBgBorderRadius: 4,
+    labelBgStyle: { fill: '#FFCC00', color: '#fff', fillOpacity: 0.7 },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  }
+
+  useEffect(()=>{
+    if(addnode){
+      const findFirstNode = nodes.find(item=>item.id===initialEdge.target)
+      setEdges((eds) => eds.concat({
+        ...initialEdge,
+        // source: parentNode.id,
+      }));
+      setAddnode(false);
+      setParentNode(null);
+    }
+    if(addChildeNode){
+      setEdges((eds) => eds.concat({
+        id: String(parseInt(Math.random(100000000)*1000000)),
+        source: parentNode.id,
+        target: nodes[nodes.length-1].id,
+        label: '+',
+        labelBgPadding: [8, 4],
+        labelBgBorderRadius: 4,
+        labelBgStyle: { fill: '#FFCC00', color: '#fff', fillOpacity: 0.7 },
+        markerEnd: {
+          type: MarkerType.ArrowClosed,
+        },
+      }));
+      setAddChildeNode(false);
+      setParentNode(null);
+    }
+  },[nodes])
+
+//  useEffect(() => {
+//     console.log('reactFlowInstance', reactFlowInstance ? reactFlowInstance.toObject() : {})
+//   })
 
   // useEffect(() => {
   //   setNodes((nds) =>
@@ -64,7 +151,6 @@ export default () => {
     []
   );
 
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge({ ...params, type: 'step' }, eds)), []);
 
   const onDragOver = useCallback((event) => {
     event.preventDefault();
@@ -105,6 +191,30 @@ export default () => {
     console.log('test');
   }
 
+  const handleEdgeClick = (param, data) => {
+    console.log(data);
+    const findSourceNode = nodes.find((item)=>item.id===data.source);
+    setNodes((nds) => nds.concat({...initialNodeType, 
+      // position:{ x: findSourceNode.position.x, y: findSourceNode.position.y+100}, 
+      data:{ parentId: data.target, ...initialNodeType.data }}));
+    setParentNode(findSourceNode);
+    setAddnode(true);
+  }
+
+  const handleNodeClick = (e, data) => {
+    const filterNodeswithSameSource = nodes.filter((node)=>node?.data?.parentId===data?.id);
+    setNodes((nds) => nds.concat({
+      id : getId(),
+      type : 'default',
+      position : { x: data.position.x+filterNodeswithSameSource.length*160, y: data.position.y+100},
+      data: { label: 'New Node', parentId: data.id },
+      width: 150,
+    }));
+    setAddChildeNode(true);
+    setParentNode(data);
+  }
+
+
   return (
     <Layout title="Flow" description="Flow" key="Flow">
       <div className="dndflow">
@@ -123,12 +233,15 @@ export default () => {
             onDragOver={onDragOver}
             onEdgeUpdate={onEdgeUpdate}
             deleteKeyCode='Delete'
-            onNodeClick={test}
+            onEdgeClick={handleEdgeClick}
+            onNodeClick={handleNodeClick}
             fitView
+            fitViewOptions={fitViewOptions}
           >
             {/* <AddButton /> */}
             <Background variant="lines" />
             <Controls />
+            <MiniMap />
           </ReactFlow>
         </div>
       </ReactFlowProvider>
