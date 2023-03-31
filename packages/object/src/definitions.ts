38436b4ef7ae61a678dfae6b5eb2281a762d9a26
Ahmed Medhat Tawfiq <ahmed.medhat.tawfiq@gmail.com>
2023-03-31 21:39:26 +0200

    add objects methods (#15)

--- packages/object/src/definitions.ts
+++ packages/object/src/definitions.ts
@@ -2,25 +2,66 @@ import { RuleDefinition } from "@jsonlang/core";
 
 export const getDefinition: RuleDefinition  = {
   identifier: { name: 'Get' },
-  inputs: { path: { type: 'array' }, defaultValue: { type: 'any' }, data: { type: 'object' } },
-  output: { type: 'boolean' }
+  inputs: { path: { type: 'string' }, data: { type: 'object' }, defaultValue: { type: 'any' } },
+  output: { type: 'any' }
 };
 
 export const setDefinition: RuleDefinition  = {
   identifier: { name: 'Set' },
-  inputs: { path: { type: 'array' }, value: { type: 'any' }, data: { type: 'object' } },
+  inputs: { path: { type: 'string' }, data: { type: 'object' }, value: { type: 'any' } },
   output: { type: 'boolean' }
 };
 
 export const updateDefinition: RuleDefinition  = {
   identifier: { name: 'Update' },
-  inputs: { path: { type: 'array' }, value: { type: 'any' }, data: { type: 'object' } },
+  inputs: { path: { type: 'string' }, data: { type: 'object' }, value: { type: 'any' } },
   output: { type: 'boolean' }
 };
 
 export const deleteDefinition: RuleDefinition  = {
   identifier: { name: 'Delete' },
-  inputs: { path: { type: 'array' },  data: { type: 'object' } },
+  inputs: { path: { type: 'string' },  data: { type: 'object' } },
   output: { type: 'boolean' }
 };
 
+export const omitDefinition: RuleDefinition  = {
+  identifier: { name: 'Omit' },
+  inputs: { paths: { type: 'array' }, data: { type: 'object' } },
+  output: { type: 'boolean' }
+};
+
+export const pickDefinition: RuleDefinition  = {
+  identifier: { name: 'Pick' },
+  inputs: { paths: { type: 'array' }, data: { type: 'object' } },
+  output: { type: 'boolean' }
+};
+
+export const copyDefinition: RuleDefinition  = {
+  identifier: { name: 'Copy' },
+  inputs: { data: { type: 'object' } },
+  output: { type: 'object' }
+};
+
+export const keysDefinition: RuleDefinition  = {
+  identifier: { name: 'Copy' },
+  inputs: { data: { type: 'object' } },
+  output: { type: 'array' }
+};
+
+export const valuesDefinition: RuleDefinition  = {
+  identifier: { name: 'Copy' },
+  inputs: { data: { type: 'object' } },
+  output: { type: 'array' }
+};
+
+export const invertDefinition: RuleDefinition  = { 
+  identifier: { name: 'Invert' },
+  inputs: { data: { type: 'object' } },
+  output: { type: 'object' }
+};
+
+export const mergeDefinition: RuleDefinition  = {
+  identifier: { name: 'Merge' },
+  inputs: { data: { type: 'array', items: { type: 'object' } } },
+  output: { type: 'object' }
+};
